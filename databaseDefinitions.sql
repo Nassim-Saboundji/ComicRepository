20808a0d29039f7f288c2b744583855be51e54d1
Nassim-Saboundji <saboundn@arcade03.iro.umontreal.ca>
2021-05-04 13:57:51 -0400

    corrections for sql schemas

--- databaseDefinitions.sql
+++ databaseDefinitions.sql
@@ -1,8 +1,8 @@
-CREATE TABLE Authors (
-    AuthorsId INT NOT NULL AUTO_INCREMENT,
-    FirstName VARCHAR(255),  -- This could be null if the authors uses a pen name
+CREATE TABLE Author (
+    AuthorId INT NOT NULL AUTO_INCREMENT,
+    FirstName VARCHAR(255),
     LastName VARCHAR(255) NOT NULL,
-    PRIMARY KEY (AuthorsId)
+    PRIMARY KEY (AuthorId)
 );
 
 CREATE TABLE Comic (
@@ -11,22 +11,25 @@ CREATE TABLE Comic (
     ComicPosterPath VARCHAR(255) NOT NULL,
     Synopsis TEXT NOT NULL,
     ComicViews INT NOT NULL, 
+    AuthorId INT,
     PRIMARY KEY (ComicId),
-    FOREIGN KEY (AuthorsId) REFERENCES Authors(AuthorsId)
+    FOREIGN KEY (AuthorId) REFERENCES Author(AuthorId)
 );
 
 CREATE TABLE Chapter (
     ChapterId INT NOT NULL AUTO_INCREMENT,
     ChapterTitle VARCHAR(255) NOT NULL,
     ChapterViews INT NOT NULL,
+    ComicId INT,
     PRIMARY KEY (ChapterId),
-    FOREIGN KEY (ComicId) REFERENCES Comic(ComicId),
+    FOREIGN KEY (ComicId) REFERENCES Comic(ComicId)
 );
 
 CREATE TABLE ComicPage (
     pageNumber INT NOT NULL,
     pagePath VARCHAR(255) NOT NULL,
     pageViews INT NOT NULL,
+    ChapterId INT,
     FOREIGN KEY (ChapterId) REFERENCES Chapter(ChapterId),
     PRIMARY KEY (ChapterId, pageNumber)
-)
+);
