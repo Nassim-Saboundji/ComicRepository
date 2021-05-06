521d9e095b80ffd094688839282fa25cb0961de4
Nassim-Saboundji <nassimsaboundji@gmail.com>
2021-05-06 12:53:50 -0400

    fixed an error related to storage for uploads

--- server.js
+++ server.js
@@ -19,47 +19,56 @@ that represents the comic as a whole).
 This route will return a json object which will tell the client
 if the operation was successful or not.
 */
-var validated = false;
+
+
+
+
 var message = "";
-const upload = multer(
-    { 
-        dest: 'uploads/',
-        fileFilter: (req, file, cb) => {
-            let title = req.body.title;
-            let synopsis = req.body.synopsis;
+function addComicFilter(req, file, cb) {
+    let title = req.body.title;
+    let synopsis = req.body.synopsis;
 
-            if (!validator.isAlphanumeric(title)) {
-                cb(null, false);
-                message = "Title is invalid";
-                return;
-            }
-
-            if (!validator.isAlphanumeric(synopsis)) {
-                cb(null, false);
-                message = "Synopsis is invalid";
-                return;
-            }
-
-            if (file.mimetype == "image/png" || file.mimetype == "image/jpg" || file.mimetype == "image/jpeg") {
-              validated = true;
-              cb(null, true);
-            } else {
-              cb(null, false);
-              message = "Only png, jpg, jpeg are accepted";
-              return;
-            }
-        }
-    }
-);
-
-
-app.post('/profile', upload.any(), function (req, res, next) {
-    if (!validated) {
-        res.json({message: message});
+    if (!validator.isAlphanumeric(title)) {
+        cb(null, false);
+        message = "Title is invalid";
+        return;
     }
 
-    db.client.connect();
-    
+    if (!validator.isAlphanumeric(synopsis)) {
+        cb(null, false);
+        message = "Synopsis is invalid";
+        return;
+    }
+
+    if (file.mimetype == "image/png" || file.mimetype == "image/jpg" || file.mimetype == "image/jpeg") {
+        message = "Upload was successful";
+        cb(null, true);
+    } else {
+        cb(null, false);
+        message = "Only png, jpg, jpeg are accepted";
+        return;
+    }
+}
+
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads/')
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.fieldname + '-' + Date.now())
+    }
+});
+
+
+var addComicUpload = multer({
+    storage: storage,
+    fileFilter : (req, file, cb) => { addComicFilter(req, file, cb) },
+});
+
+
+
+app.post('/profile', addComicUpload.single('poster'), function (req, res, next) {
+    res.json({message: message});
 });
 
 app.listen(port, () => {
