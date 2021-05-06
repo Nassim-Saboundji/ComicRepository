4320174a7a740ef6854ae7a3f5e4d7ebdb19322d
Nassim-Saboundji <nassimsaboundji@gmail.com>
2021-05-06 12:10:10 -0400

    written code for handling file upload and validation

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 const db = require('./dummyDBCredentials');
 const express = require('express');
 const validator = require('validator');
+const multer = require('multer');
+
 
 const app = express();
-const port = process.env.PORT || 8080;
+const port = 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 
 /*
 This routes allows the user to add a comic to the comicRepo
@@ -18,26 +19,49 @@ that represents the comic as a whole).
 This route will return a json object which will tell the client
 if the operation was successful or not.
 */
-app.post('/addComic', (req, res) => {
-    let title = req.body.title;
-    let synopsis = req.body.synopsis;
-    let posterImage = req.body.posterImage;
+var validated = false;
+var message = "";
+const upload = multer(
+    { 
+        dest: 'uploads/',
+        fileFilter: (req, file, cb) => {
+            let title = req.body.title;
+            let synopsis = req.body.synopsis;
 
-    if (!validator.isAlphanumeric(title)) {
-        res.json({
-            "error" : "Invalide title" 
-        });
+            if (!validator.isAlphanumeric(title)) {
+                cb(null, false);
+                message = "Title is invalid";
+                return;
+            }
+
+            if (!validator.isAlphanumeric(synopsis)) {
+                cb(null, false);
+                message = "Synopsis is invalid";
+                return;
+            }
+
+            if (file.mimetype == "image/png" || file.mimetype == "image/jpg" || file.mimetype == "image/jpeg") {
+              validated = true;
+              cb(null, true);
+            } else {
+              cb(null, false);
+              message = "Only png, jpg, jpeg are accepted";
+              return;
+            }
+        }
+    }
+);
+
+
+app.post('/profile', upload.any(), function (req, res, next) {
+    if (!validated) {
+        res.json({message: message});
     }
 
-    if (!validator.isAlphanumeric(synopsis)) {
-        res.json({
-            "error": "Invalide synopsis"
-        });
-    }
-
+    db.client.connect();
     
 });
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
-})
+});
