faaaa8f890913d572897d957471fb4a8ea02b8da
Nassim-Saboundji <nassimsaboundji@gmail.com>
2021-05-15 17:51:47 -0400

    working on the frontend

--- frontend/comics.js
+++ frontend/comics.js
@@ -1,13 +1,37 @@
-document.addEventListener("DOMContentLoaded", () => {
-    getComics();
+window.addEventListener("DOMContentLoaded", () => {
+    generateHomePage();
 });
+
+//This is our router
+window.addEventListener('hashchange', () => {
+    
+    let currentHash = window.location.hash;
+    let currentHashArray = currentHash.split('/');
+    
+    if (currentHashArray[1] == "comics") {
+        document.getElementById("root").replaceChildren();
+        generateComicPage(currentHashArray[2]);
+    }
+    
+    if (currentHashArray[1] == "upload") {
+
+    }
+
+    if (currentHashArray[1] == "login") {
+
+    }
+
+});
+
+async function generateHomePage() {
+    let data = await getComics();
+    generateComicSection(data);
+}
 
 
 async function getComics() {
     const response = await fetch('http://localhost:3000/comics');
-    const data = await response.json();
-    generateComicSection(data);
-
+    return response.json();
 }
 
 function generateComicSection(data) {
@@ -18,16 +42,34 @@ function generateComicSection(data) {
 
 
 function comicCard(comicData) {
+    let resultSection = document.getElementById('resultSection');
     let card = document.createElement('div');
     let title = document.createElement('p');
     let poster = document.createElement('img');
 
     title.innerText = comicData.comic_title;
     poster.src = "http://localhost:3000/static/" + comicData.comic_poster;
-    card.id = comicData.comic_id;
+
+    //add an event listener to every card
+    card.addEventListener('click', function() {
+        // clean the DOM 
+        document.getElementById("root").replaceChildren(); 
+        window.location.hash = "/comics/" + comicData.comic_id;
+        generateComicPage(comicData.comic_id);
+    });
 
     card.appendChild(poster);
     card.appendChild(title);
-    document.getElementById('resultSection').appendChild(card);
+    resultSection.appendChild(card);
     
+
 }
+
+
+
+
+function generateComicPage(comicId) {
+    console.log(comicId);
+}
+
+
