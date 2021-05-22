1c9b6b27bd4a69fa20e2b8f5fc98ece7e6bbf89d
Nassim-Saboundji <nassimsaboundji@gmail.com>
2021-05-22 16:22:33 -0400

    finished tests for all POST routes

--- tests/test.js
+++ tests/test.js
@@ -125,6 +125,88 @@ describe("POST /addComic", function () {
     });
 });
 
+describe("POST /addChapter", function () {
+    it("Should add a chapter to an existing comic.", async function () {
+        const comicId = await db.pool.query(
+            "SELECT comic_id FROM comic WHERE comic_title='Calvin and Hobbes'"
+        );
+
+        const id = await comicId.rows[0]["comic_id"] + "";
+        const data = {
+            comicId: id,
+            chapterNumber: "1",
+            chapterTitle: "First Chapter"
+        };
+        
+        var mockApp = express();
+        mockApp.use(session({ 
+            secret: secret.mySecret,
+            cookie: { maxAge: 3600000 }, //A user session expires after 60 minutes
+            resave: true,
+            saveUninitialized: true
+        }));
+        mockApp.all('*', function(req, res, next) {
+            req.session.logged = true;
+            next();
+        });
+
+        mockApp.use(server.app);
+        
+        const response = await request(mockApp)
+        .post('/addChapter')
+        .set({
+            'Content-Type': 'application/json',
+        })
+        .field(data)
+        .attach('pages', 'ch1-1.jpg')
+        .attach('pages', 'ch1-2.jpg')
+        .attach('pages', 'ch1-3.jpeg')
+        .attach('pages', 'ch1-4.jpg');
+
+        expect(response.body.message).to.eql('Upload was successful.');
+    });
+});
+
+
+describe("POST /removeChapter", function () {
+    it("Should remove the chapter added in the previous test.", async function () {
+        this.timeout(3000);
+
+        const comicId = await db.pool.query(
+            "SELECT comic_id FROM comic WHERE comic_title='Calvin and Hobbes'"
+        );
+
+        const id = await comicId.rows[0]["comic_id"] + "";
+        const data = {
+            comicId: id,
+            chapterNumber: "1"
+        };
+        
+        var mockApp = express();
+        mockApp.use(session({ 
+            secret: secret.mySecret,
+            cookie: { maxAge: 3600000 }, //A user session expires after 60 minutes
+            resave: true,
+            saveUninitialized: true
+        }));
+        mockApp.all('*', function(req, res, next) {
+            req.session.logged = true;
+            next();
+        });
+
+        mockApp.use(server.app);
+        
+        const response = await request(mockApp)
+        .post('/removeChapter')
+        .set({
+            'Content-Type': 'application/json',
+        })
+        .send(data);
+
+        expect(response.body.message).to.eql('Chapter successfully deleted.');
+    });
+});
+
 describe("POST /removeComic" , function () {
     it("Should remove the comic that was added in the previous test.", async function () {
         const comicIdToRemove = await db.pool.query(
@@ -159,3 +241,5 @@ describe("POST /removeComic" , function () {
         
     });
 });
+
+
