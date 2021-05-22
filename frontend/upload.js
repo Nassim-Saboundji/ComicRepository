deb91a49f48e74e2e261c0f4df2d640b70a90752
Nassim-Saboundji <70408318+Nassim-Saboundji@users.noreply.github.com>
2021-05-22 17:25:29 -0400

    Delete frontend directory

--- frontend/upload.js
+++
@@ -1,7 +0,0 @@
-
-async function send(e, form) {
-    e.preventDefault();
-    const response = await (await fetch(form.action, {method:'post', body: new FormData(form)})).json();
-    console.log(response);
-    return false;
-}
