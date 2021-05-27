57697de605005e7af974fcbf460899e572ebfc5b
Nassim-Saboundji <70408318+Nassim-Saboundji@users.noreply.github.com>
2021-05-26 22:08:37 -0400

    Update addChapterManager.js

--- src/managers/addChapterManager.js
+++ src/managers/addChapterManager.js
@@ -80,7 +80,7 @@ async function addChapterFilter(req, file, cb) {
 
 var storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, 'src/uploads/');
+        cb(null, './uploads');
     },
     filename: function (req, file, cb) {
         let fileType = file.mimetype.split('/');
